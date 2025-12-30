3615fde76eedc72497dfd1e04a293241f074ffa4
JSolaMundum <67022075+JSolaMundum@users.noreply.github.com>
2025-12-30 13:21:42 +0300

    chore: Reorder `CurrentWork` and `FocusAreas` components.

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <main>
         <Hero />
         <About />
-        <CurrentWork />
         <FocusAreas />
+        <CurrentWork />
         <Experience />
         <Contact />
       </main>
