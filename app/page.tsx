0092c6914941c4fcad0f85f7e2555a7e88065669
JSolaMundum <67022075+JSolaMundum@users.noreply.github.com>
2025-12-30 13:08:56 +0300

    refactor: remove Projects and Writing components from homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,6 @@ export default function Home() {
         <CurrentWork />
         <FocusAreas />
         <Experience />
-        <Projects />
-        <Writing />
         <Contact />
       </main>
       <Footer />
